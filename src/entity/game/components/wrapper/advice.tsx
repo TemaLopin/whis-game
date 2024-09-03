b5258cab358b33bbd4b11d2e3df1c4492bf51fb9
Tema <108663530+TemaLopin@users.noreply.github.com>
2024-09-03 15:29:14 +0300

    fixed height

--- src/entity/game/components/wrapper/advice.tsx
+++ src/entity/game/components/wrapper/advice.tsx
@@ -1,16 +1,14 @@
 import s from './style.module.scss'
-import { ReactNode } from 'react'
+import {ReactNode} from 'react'
 import clsx from 'clsx'
 import { useParams } from 'react-router-dom'
 import useWindowDimensions from '../../../../shared/hooks/useWindowDimensions'
 
 const AdviceWrapper = ({ children }: { children: ReactNode }) => {
-  const { height } = useWindowDimensions()
   const { type } = useParams()
-console.log(document.body.scrollHeight, document.documentElement.clientHeight)
   return (
     <div
-      className={clsx('background', s.wrapper, type === 'cat' ? s.wrapper_cat : s.wrapper_dog)}
+      className={clsx('background', s.wrapper, type === 'cat' ? s.wrapper_cat : s.wrapper_dog) }
       // style={{ height: document.body.scrollHeight > document.documentElement.clientHeight  ? '100vh' : `100%` }}
     >
       {children}
